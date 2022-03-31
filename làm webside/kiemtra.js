b4dc3ba064a59f203366b952e645c7f02b96e0ab
Miracle2712 <100839563+Miracle2712@users.noreply.github.com>
2022-03-31 09:56:38 +0700

    test1

--- làm webside/kiemtra.js
+++ làm webside/kiemtra.js
@@ -11,3 +11,14 @@ function validate() {
         alert("Đăng nhập không thành công")
     }
 }
+
+function dangki() {
+    let u = document.getElementById("username").value;
+    let p = document.getElementById("password").value;
+    let q = document.getElementById("password-repeat").value;
+    if(u == "admin"&& p == "123" && q == "123") {
+        alert("Bạn đã đăng kí thành công!");
+
+        window.location = "index.html"
+    }
+}
